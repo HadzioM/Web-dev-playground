1200c2a0ffb03c9ad3be85c859142e2df64b06f1
Mateusz Hadziewicz <mateusz.hadziewicz@gmail.com>
2024-11-27 12:09:09 +0000

    vue + primevue + tailwind playground

---
+++ VueTailwindPrimeVuePlayground/vue-playground/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+import SelectButton from 'primevue/selectbutton';
+import { ref } from 'vue';
+
+const value = ref('One-Way');
+const options = ref(['One-Way', 'Return']);
+</script>
+
+<template>
+  <div>
+    <SelectButton v-model="value" :options="options" />
+  </div>
+</template>
+
+<style scoped></style>
